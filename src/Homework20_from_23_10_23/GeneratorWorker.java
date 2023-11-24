100bffd14876f89b3771c2fc0d1f8335e831aeb5
Vera <inginer719@gmail.ru>
2023-11-24 09:39:27 -0500

    in work

--- src/Homework20_from_23_10_23/GeneratorWorker.java
+++ src/Homework20_from_23_10_23/GeneratorWorker.java
@@ -1,11 +1,14 @@
 package Homework20_from_23_10_23;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class GeneratorWorker {
     public static void main(String[] args) throws IOException {
+
+        List<Worker> generatorWorker = new ArrayList<>();
         Random random = new Random();
 
         File workerFemale = new File("src/Files/generatorFemale.txt");
@@ -36,7 +39,20 @@ public class GeneratorWorker {
         System.out.println();
         System.out.println(workerMales);
 
+for (int i = 0; i < 5000; i++){
+    int minSalary = random.nextInt(550, 1000);
+    int maxSalary = random.nextInt(1100, 6000);
 
+    String generatedFemale = workerFemales.get(random.nextInt(0, 100)) + " " + minSalary + " " + maxSalary + " " + GenderTypes.FEMALE;
+    bufferedWriterWorkerFemale.write(generatorWorkerFemale);
+    bufferedWriterWorkerFemale.newLine();
+    bufferedWriterWorkerFemale.flush();
+
+    String generatedMale = workerMales.get(random.nextInt(0, 100)) + " " + minSalary + " " + maxSalary + " " + GenderTypes.MALE;
+    bufferedWriterWorkerMale.write(generatedMale);
+    bufferedWriterWorkerMale.newLine();
+    bufferedWriterWorkerMale.flush();
+}
     }
 
 }
