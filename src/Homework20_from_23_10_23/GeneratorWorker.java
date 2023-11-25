c26502e4d6161995388fc411d4ae01bed80ddcfc
Vera <inginer719@gmail.ru>
2023-11-25 11:06:57 -0500

    in work

--- src/Homework20_from_23_10_23/GeneratorWorker.java
+++ src/Homework20_from_23_10_23/GeneratorWorker.java
@@ -11,20 +11,15 @@ public class GeneratorWorker {
         List<Worker> generatorWorker = new ArrayList<>();
         Random random = new Random();
 
-           File generatorFemale = new File("src/Files/generatorFemale.txt");
-           File generatorMale = new File("src/Files/generatorMale.txt");
+        String generatorFemale = "src/Files/generatorFemale.txt";
+        String generatorMale = "src/Files/generatorMale.txt";
 
         FileReader fileReaderWorkerFemale = new FileReader(generatorFemale);
         FileReader fileReaderWorkerMale = new FileReader(generatorMale);
 
-        FileWriter fileWriterWorkerFemale = new FileWriter(String.valueOf(fileReaderWorkerFemale));
-        FileWriter fileWriterWorkerMale = new FileWriter(String.valueOf(fileReaderWorkerMale));
-
         BufferedReader bufferedReaderWorkerFemale = new BufferedReader(fileReaderWorkerFemale);
         BufferedReader bufferedReaderWorkerMale = new BufferedReader(fileReaderWorkerMale);
 
-        BufferedWriter bufferedWriterWorkerFemale = new BufferedWriter(fileWriterWorkerFemale);
-        BufferedWriter bufferedWriterWorkerMale = new BufferedWriter(fileWriterWorkerMale);
 
         List<String> workerFemales = bufferedReaderWorkerFemale.lines()
                 .toList();
@@ -36,20 +31,10 @@ public class GeneratorWorker {
         System.out.println();
         System.out.println(workerMales);
 
-for (int i = 0; i < 5000; i++){
-    int minSalary = random.nextInt(550, 1000);
-    int maxSalary = random.nextInt(1100, 6000);
+        for (int i = 0; i < workerFemales.size(); i++){
+            String worker = workerFemales.get(i);
 
-    String generatedFemale = workerFemales.get(random.nextInt(0, 100)) + " " + minSalary + " " + maxSalary + " " + GenderTypes.FEMALE;
-    bufferedWriterWorkerFemale.write(String.valueOf(generatedFemale));
-    bufferedWriterWorkerFemale.newLine();
-    bufferedWriterWorkerFemale.flush();
-
-    String generatedMale = workerMales.get(random.nextInt(0, 100)) + " " + minSalary + " " + maxSalary + " " + GenderTypes.MALE;
-    bufferedWriterWorkerMale.write(generatedMale);
-    bufferedWriterWorkerMale.newLine();
-    bufferedWriterWorkerMale.flush();
-}
+        }
     }
 
 }
